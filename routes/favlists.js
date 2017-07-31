94216e4cb43901aa82e953725e7e0b092ae55ab5
Michael Martinez <michael.martinez@galvanize.com>
2017-07-31 11:43:46 -0700

    added post route

--- routes/favlists.js
+++ routes/favlists.js
@@ -5,24 +5,24 @@ const knex = require('../knex');
 router.route('/')
   .get((req, res) => {
     knex('favlists')
-        .orderBy('name')
-        .then(favLists => res.json(favLists))
-        .catch(err => {
-            res.sendStatus(404);
-            next(err);
-        });
+      .orderBy('name')
+      .then(favLists => res.json(favLists))
+      .catch(err => res.sendStatus(404));
   })
   .post((req, res) => {
-    res.send('this will create a favlist!!!');
+    knex('favlists')
+      .insert({ name: req.body.name }, '*')
+      .then(result => res.json(result[0]))
+      .catch(err => res.sendStatus(400));
   });
 
 router.route('/:id')
   .get((req, res) => {
-    console.log('this is req', req.params);
     knex('favlists')
       .where('id', req.params.id)
       .first()
-      .then(favlist => favlist ? res.json(favlist) : res.status(404).send('Not Found'));
+      .then(favlist =>
+        favlist ? res.json(favlist) : res.status(404).send('Not Found'));
   })
   .post((req, res) => {
     res.send('this will update a specific favlist');
