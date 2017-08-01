c7fe3cbbc59b4745d09335ebc714faffa7ac471e
Michael Martinez <michael.martinez@galvanize.com>
2017-07-31 22:06:31 -0700

    refactored seed files and added sample data

--- seeds/00_favlists.js
+++ seeds/00_favlists.js
@@ -1,17 +1,20 @@
-exports.seed = function(knex, Promise) {
-    // Deletes ALL existing entries
-    return knex('favlists').del()
-        .then(()  => {
-            return Promise.all([
-                // Inserts seed entries
-                knex('favlists').insert({
-                    id: 1,
-                    name: 'Horror Movies List',
-                    description: 'The scariest Movies of All Time',
-                    created_at: Date.now(),
-                    updated_at: Date.now()
-                }),
-            ]);
-        })
-        // .then(() => knex.raw("SELECT setval('favlists_id_seq', (SELECT MAX(id) FROM favlists))"));
-};
+
+exports.seed = knex => knex('favlists').del()
+  .then(() =>
+  knex('favlists')
+    .insert([
+      {
+        id: 1,
+        name: 'Horror Movies List',
+        description: 'The scariest Movies of All Time',
+        created_at: new Date('2017-07-31 12:12:12 UTC'),
+        updated_at: new Date('2017-07-31 12:12:12 UTC')
+      }, {
+        id: 2,
+        name: 'Comedy Movies List',
+        description: 'The funniest Movies of All Time',
+        created_at: new Date('2017-07-31 12:12:12 UTC'),
+        updated_at: new Date('2017-07-31 12:12:12 UTC')
+        }
+    ])
+  );
--- seeds/01_entries.js
+++ seeds/01_entries.js
@@ -1,15 +1,50 @@
-exports.seed = function(knex, Promise) {
-    return knex('entries').del()
-        .then(()  => {
-            return Promise.all([
-                knex('entries').insert({
-                    id: 1,
-                    favlistId: 1,
-                    name: 'Halloween',
-                    description: 'The classic John Carpenter Film',
-                    created_at: Date.now(),
-                    updated_at: Date.now()
-                }),
-            ]);
-        });
-};
+
+exports.seed = knex => knex('entries').del()
+  .then(() =>
+  knex('entries')
+    .insert([
+      {
+        id: 1,
+        favlistId: 1,
+        name: 'Halloween',
+        description: 'The classic John Carpenter Film',
+        created_at: new Date('2017-07-31 12:12:12 UTC'),
+        updated_at: new Date('2017-07-31 12:12:12 UTC')
+      },{
+        id: 2,
+        favlistId: 1,
+        name: 'Friday the 13th',
+        description: 'The one without Jason',
+        created_at: new Date('2017-07-31 12:12:12 UTC'),
+        updated_at: new Date('2017-07-31 12:12:12 UTC')
+      },{
+        id: 3,
+        favlistId: 1,
+        name: 'Poltergeist',
+        description: 'A Film about a snarky ghost',
+        created_at: new Date('2017-07-31 12:12:12 UTC'),
+        updated_at: new Date('2017-07-31 12:12:12 UTC')
+      }, {
+        id: 4,
+        favlistId: 2,
+        name: 'Airplane!',
+        description: 'The best parady film',
+        created_at: new Date('2017-07-31 12:12:12 UTC'),
+        updated_at: new Date('2017-07-31 12:12:12 UTC')
+      }, {
+        id: 5,
+        favlistId: 2,
+        name: 'Monty Python and the Holy Grail',
+        description: 'A true classic',
+        created_at: new Date('2017-07-31 12:12:12 UTC'),
+        updated_at: new Date('2017-07-31 12:12:12 UTC')
+      }, {
+        id: 6,
+        favlistId: 2,
+        name: 'Anchorman',
+        description: 'Stay Classy San Diego',
+        created_at: new Date('2017-07-31 12:12:12 UTC'),
+        updated_at: new Date('2017-07-31 12:12:12 UTC')
+      },
+    ])
+  );
