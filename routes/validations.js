983c5304535015cd32a31f50bbc20daed455f649
Michael Martinez <michael.martinez@galvanize.com>
2017-08-01 23:30:40 -0700

    added admin authorization middleware

--- routes/validations.js
+++ routes/validations.js
@@ -1,7 +1,11 @@
 const express = require('express');
 
-function validation(req, res, next) {
-  isNaN(req.params.id) ? res.status(404).send('Not Found') : next();
+const numValidation = (req, res, next) => {
+  isNaN(req.params.id) ? res.status(404).send('Not Found; Not a number') : next();
 }
 
-module.exports = validation;
+const authValidation = (req, res, next) => {
+  req.headers.admin !== 'true' ? res.status(403).send('YOU ARE FORBIDDEN') : next();
+}
+
+module.exports = { numValidation, authValidation };
