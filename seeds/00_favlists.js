133ee0663a83d87f27184d04775fc256f738cba2
Michael Martinez <michael.martinez@galvanize.com>
2017-08-01 21:05:28 -0700

    refactored date timestamps

--- seeds/00_favlists.js
+++ seeds/00_favlists.js
@@ -7,14 +7,14 @@ exports.seed = knex => knex('favlists').del()
         id: 1,
         name: 'Horror Movies List',
         description: 'The scariest Movies of All Time',
-        created_at: new Date('2017-07-31 12:12:12 UTC'),
-        updated_at: new Date('2017-07-31 12:12:12 UTC')
+        created_at: new Date().toString(),
+        updated_at: new Date().toString()
       }, {
         id: 2,
         name: 'Comedy Movies List',
         description: 'The funniest Movies of All Time',
-        created_at: new Date('2017-07-31 12:12:12 UTC'),
-        updated_at: new Date('2017-07-31 12:12:12 UTC')
+        created_at: new Date().toString(),
+        updated_at: new Date().toString()
         }
     ])
   );
--- seeds/02_comments.js
+++ seeds/02_comments.js
@@ -8,36 +8,36 @@ exports.seed = knex => knex('comments').del()
         entryId: 1,
         comment: 'Halloween is not that great of a movie',
         author: 'Anon',
-        created_at: new Date('2017-07-31 12:12:12 UTC'),
-        updated_at: new Date('2017-07-31 12:12:12 UTC')
+        created_at: new Date().toString(),
+        updated_at: new Date().toString()
       }, {
         id: 2,
         entryId: 1,
         comment: `Halloween is John Carpenter's best film!`,
         author: 'Anon',
-        created_at: new Date('2017-07-31 12:12:12 UTC'),
-        updated_at: new Date('2017-07-31 12:12:12 UTC')
+        created_at: new Date().toString(),
+        updated_at: new Date().toString()
       },{
         id: 3,
         entryId: 2,
         comment: `Wait....., Jason is not in this one?`,
         author: 'Anon1',
-        created_at: new Date('2017-07-31 12:12:12 UTC'),
-        updated_at: new Date('2017-07-31 12:12:12 UTC')
+        created_at: new Date().toString(),
+        updated_at: new Date().toString()
       }, {
         id: 4,
         entryId: 4,
         comment: `The funniest movie ever made`,
         author: 'Anon2',
-        created_at: new Date('2017-07-31 12:12:12 UTC'),
-        updated_at: new Date('2017-07-31 12:12:12 UTC')
+        created_at: new Date().toString(),
+        updated_at: new Date().toString()
       }, {
         id: 5,
         entryId: 4,
         comment: `Surely, you can't be serious?`,
         author: 'Anon',
-        created_at: new Date('2017-07-31 12:12:12 UTC'),
-        updated_at: new Date('2017-07-31 12:12:12 UTC')
+        created_at: new Date().toString(),
+        updated_at: new Date().toString()
       },
     ])
   );
